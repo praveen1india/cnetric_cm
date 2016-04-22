a5a7f9c4827e4a7b9c3365c74bf72a710fb0435c
ravi <kosaravi@gmail.com>
2016-04-22 11:31:34 +0530

    view updates

---
+++ administrator/components/com_wcs_pim/views/dashboard/tmpl/default.php
@@ -0,0 +1,72 @@
+<link href='https://fonts.googleapis.com/css?family=Jaldi:400,700' rel='stylesheet' type='text/css'>
+
+	<link rel="stylesheet" href="<?php echo JURI::Root();?>multi-level-accordion-menu/css/style.css"> <!-- Resource style -->
+<?php
+/**
+ * @version    CVS: 1.0.0
+ * @package    Com_Wcs_pim
+ * @author     Praveen <mantu.mnt@gmail.com>
+ * @copyright  2016 demo
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access
+defined('_JEXEC') or die;
+
+JHtml::addIncludePath(JPATH_COMPONENT . '/helpers/html');
+JHtml::_('bootstrap.tooltip');
+JHtml::_('behavior.multiselect');
+JHtml::_('formbehavior.chosen', 'select');
+
+// Import CSS
+$document = JFactory::getDocument();
+$document->addStyleSheet(JUri::root() . 'administrator/components/com_wcs_pim/assets/css/wcs_pim.css');
+$document->addStyleSheet(JUri::root() . 'media/com_wcs_pim/css/list.css');
+
+$user      = JFactory::getUser();
+$userId    = $user->get('id');
+
+
+$sortFields = $this->getSortFields();
+?>
+<script type="text/javascript">
+	jQuery(document).ready(function () {
+		jQuery('#clear-search-button').on('click', function () {
+			jQuery('#filter_search').val('');
+			jQuery('#adminForm').submit();
+		});
+	});
+</script>
+
+
+
+
+<div id="content">
+
+<div class="text-center">
+	<a class="quick-btn" href="index.php?option=com_wcs_pim&view=pimproducts">
+		<i class="fa fa-product-hunt fa-2x"></i>
+		<span>Product management</span>
+		<span class="label label-default">15000</span>
+	</a>
+	
+	<a class="quick-btn" href="index.php?option=com_wcs_pim&view=categories">
+		<i class="fa fa-folder fa-2x"></i>
+		<span>Category management</span>
+		<span class="label label-default">120</span>
+	</a>
+	
+	<a class="quick-btn" href="#">
+		<i class="fa fa-tasks fa-2x"></i>
+		<span>Manage Attributes</span>
+		<span class="label label-default">120</span>
+	</a>
+	
+	<a class="quick-btn" href="#">
+		<i class="fa fa-upload	 fa-2x"></i>
+		<span>Bulk upload</span>
+		<span class="label label-default">120</span>
+	</a>
+</div>
+
+</div>
